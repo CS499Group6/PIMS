fc6a3bd916799cd02ecea4ddcfd93c4c82817e96
Ina Carter <ina.carter@gmail.com>
2016-04-14 17:00:20 -0500

    room assignment

---
+++ PIMS_Final/PIMSv2/GUI/OfficeStaff/RoomAssignmentGrid.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PIMS
+{
+    partial class RoomAssignmentGrid
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.roomAssignmentDataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.bedsAvailable = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.roomNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.facility = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.roomAssignmentDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // roomAssignmentDataGridView
+            // 
+            this.roomAssignmentDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.roomAssignmentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.roomAssignmentDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.bedsAvailable,
+            this.roomNumber,
+            this.unit,
+            this.facility});
+            this.roomAssignmentDataGridView.Location = new System.Drawing.Point(18, 22);
+            this.roomAssignmentDataGridView.Name = "roomAssignmentDataGridView";
+            this.roomAssignmentDataGridView.RowHeadersWidth = 35;
+            this.roomAssignmentDataGridView.Size = new System.Drawing.Size(537, 362);
+            this.roomAssignmentDataGridView.TabIndex = 3;
+            this.roomAssignmentDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.billingDataGridView_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.FalseValue = "false";
+            this.Column1.HeaderText = "Select";
+            this.Column1.IndeterminateValue = "false";
+            this.Column1.Name = "Column1";
+            this.Column1.TrueValue = "true";
+            // 
+            // bedsAvailable
+            // 
+            this.bedsAvailable.HeaderText = "Number of Beds Available";
+            this.bedsAvailable.Name = "bedsAvailable";
+            // 
+            // roomNumber
+            // 
+            this.roomNumber.HeaderText = "Room Number";
+            this.roomNumber.Name = "roomNumber";
+            // 
+            // unit
+            // 
+            this.unit.HeaderText = "Unit";
+            this.unit.Name = "unit";
+            // 
+            // facility
+            // 
+            this.facility.HeaderText = "Facility";
+            this.facility.Name = "facility";
+            // 
+            // RoomAssignmentGrid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.roomAssignmentDataGridView);
+            this.Name = "RoomAssignmentGrid";
+            this.Size = new System.Drawing.Size(735, 526);
+            ((System.ComponentModel.ISupportInitialize)(this.roomAssignmentDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView roomAssignmentDataGridView;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bedsAvailable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn roomNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn facility;
+    }
+}
---
+++ PIMS_Final/PIMSv2/GUI/OfficeStaff/RoomAssignmentGrid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIMS
+{
+    public partial class RoomAssignmentGrid : UserControl
+    {
+        // Default Constructor
+        public RoomAssignmentGrid()
+        {
+            InitializeComponent();
+        }
+
+        private void billingDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            roomAssignmentDataGridView.CellValueChanged += roomAssignmentDataGridView_CellValueChanged;
+        }
+
+        private void roomAssignmentDataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
